3b0b5483ebbaed6c074ccd4274fa88dc566334c5
manjot <mssond459@gmail.com>
2024-11-09 07:54:47 -0800

    fire changing

--- App.tsx
+++ App.tsx
@@ -1,16 +1,46 @@
-import React from "react";
-import { createTheme, ThemeProvider } from "@rneui/themed";
-import Component from "./components/MyComponent";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Feather } from "@expo/vector-icons";
+import HomeScreen from "./navigations/homeScreen";
+import SettingsScreen from "./navigations/settingsSreen";
+import useTheme from "./hook/theme";
 
-const theme = createTheme({
-  lightColors: {},
-  darkColors: {},
-});
+type TabParamList = {
+  Home: undefined;
+  Settings: undefined;
+};
 
-export default function App() {
+const Tab = createBottomTabNavigator<TabParamList>();
+const App: React.FC = () => {
+  const { colors } = useTheme();
   return (
-    <ThemeProvider theme={theme}>
-      <Component />
-    </ThemeProvider>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarStyle: { backgroundColor: colors.background },
+          tabBarIcon: ({ color, size }) => {
+            let iconName: React.ComponentProps<typeof Feather>["name"];
+
+            if (route.name === "Home") {
+              iconName = "home";
+            } else if (route.name === "Settings") {
+              iconName = "settings";
+            } else {
+              iconName = "circle";
+            }
+
+            return <Feather name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "#FF6500",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-}
+};
+
+export default App;
