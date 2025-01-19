7de66c37e8a801868b1c660c750dbed3485ad41f
manjot <mssond459@gmail.com>
2025-01-20 00:04:19 +0530

    icons added

--- App.tsx
+++ App.tsx
@@ -1,34 +1,34 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import "./global.css";
-import "./gesture-handler";
+import Ionicons from "react-native-vector-icons/Ionicons";
 import HomeNavigation from "./app/navigation/Home";
 import SettingNavigation from "./app/navigation/Setting";
-import { GoHome } from "react-icons/go";
-import { GoHomeFill } from "react-icons/go";
-import { IoSettingsOutline } from "react-icons/io5";
-import { IoSettingsSharp } from "react-icons/io5";
+
 const Tab = createBottomTabNavigator();
+
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={({ route }) => ({
+          tabBarActiveTintColor: "red",
+          tabBarInactiveTintColor: "green",
           tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-  
-            if (route.name === 'Home') {
-              return focused ? <GoHome size={size} color={color} /> : <GoHomeFill size={size} color={color}/>;
-            } else if (route.name === 'Settings') {
-              return focused ? <IoSettingsOutline size={size} color={color} /> : <IoSettingsSharp size={size} color={color}/>;
+            let iconName = "";
+
+            if (route.name === "Home") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "Settings") {
+              iconName = focused ? "settings" : "settings-outline";
             }
-  
+
+            // Return the icon component
+            return <Ionicons name={iconName} size={24} color={color} />;
           },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
-          })}>
-        <Tab.Screen name="HomeNavigation" component={HomeNavigation} />
-        <Tab.Screen name="SettingNavigation" component={SettingNavigation} />
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeNavigation} />
+        <Tab.Screen name="Settings" component={SettingNavigation} />
       </Tab.Navigator>
     </NavigationContainer>
   );
